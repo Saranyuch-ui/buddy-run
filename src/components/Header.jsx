22bada7a35036ce67c70764a3b79915ff0b43d7d
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 16:15:30 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
-function Header() {
+function Header({ onLogoClick }) {
   return (
     <header className="header">
-      <div className="header-content">
+      <div
+        className="header-content"
+        onClick={onLogoClick}
+        style={{ cursor: onLogoClick ? "pointer" : "default" }}
+      >
         <img src="/logo.png" alt="Buddy Run Logo" className="logo" />
+        <div className="header-text">
+          <h1>Buddy Run</h1>
+          <p>Run Together, Better Together</p>
+        </div>
       </div>
     </header>
   );
