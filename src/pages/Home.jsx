db5cbc3869db80c05d48eacabb072c696d6daf45
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 10:42:26 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,48 @@
+import { useState } from "react";
 
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import SearchBar from "../components/SearchBar";
+import EventCard from "../components/EventCard";
+import Footer from "../components/Footer";
+
+import events from "../data/events";
+
+function Home() {
+
+  const [search, setSearch] = useState("");
+
+  const filteredEvents = events.filter((event) =>
+    event.title.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <>
+
+      <Header />
+
+      <Hero />
+
+      <SearchBar
+        search={search}
+        setSearch={setSearch}
+      />
+
+      <div className="grid">
+
+        {filteredEvents.map((event) => (
+          <EventCard
+            key={event.id}
+            event={event}
+          />
+        ))}
+
+      </div>
+
+      <Footer />
+
+    </>
+  );
+}
+
+export default Home;
