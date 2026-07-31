774b81d3e9b9ad7db521ee0e35f59db0f3efac6a
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 10:41:07 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,1 +1,13 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h2>Discover Running Events Across Thailand</h2>
 
+      <p>
+        Search your next marathon, mini marathon or fun run.
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
