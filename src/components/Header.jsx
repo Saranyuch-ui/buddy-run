842ec3b84b4f6f0fc8e4302f44707b3137b5dc8d
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 10:40:52 +0700

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>🏃 Buddy Run</h1>
+      <p>Find Your Next Running Event</p>
+    </header>
+  );
+}
 
+export default Header;
