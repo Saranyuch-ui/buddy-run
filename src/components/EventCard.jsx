23f3ee7105a9a385b84dd9e1fa8560bf6a3c37e8
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 10:41:50 +0700

    Update EventCard.jsx

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,1 +1,30 @@
+function EventCard({ event }) {
+  return (
+    <div className="card">
 
+      <img
+        src={event.image}
+        alt={event.title}
+      />
+
+      <div className="card-body">
+
+        <h3>{event.title}</h3>
+
+        <p>📅 {event.date}</p>
+
+        <p>📍 {event.location}</p>
+
+        <p>🏃 {event.distance}</p>
+
+        <button>
+          View Detail
+        </button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default EventCard;
