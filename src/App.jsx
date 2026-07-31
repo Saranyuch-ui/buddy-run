1484fc4684ee95c10c38e7d4006264d2737860db
Saranyuch-ui <startemperor@gmail.com>
2026-07-31 13:18:54 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
-  return <Home />;
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  if (selectedEvent) {
+    return (
+      <Detail
+        event={selectedEvent}
+        onBack={() => setSelectedEvent(null)}
+      />
+    );
+  }
+
+  return <Home onSelectEvent={setSelectedEvent} />;
 }
 
 export default App;
