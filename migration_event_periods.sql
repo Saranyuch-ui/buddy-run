39857fcfcb6db13ad7a10a1efb4848282a1bfad6
Saranyuch-ui <startemperor@gmail.com>
2026-08-06 15:18:41 +0700

    Create migration_event_periods.sql

---
+++ migration_event_periods.sql
@@ -0,0 +1,4 @@
+ALTER TABLE events ADD COLUMN reg_start_date TEXT;
+ALTER TABLE events ADD COLUMN reg_end_date TEXT;
+ALTER TABLE events ADD COLUMN result_start_date TEXT;
+ALTER TABLE events ADD COLUMN result_end_date TEXT;
