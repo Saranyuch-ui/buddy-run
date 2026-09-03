6ccae1bdd1c3bfaed1c7d44cc165ab5868266b21
Saranyuch-ui <startemperor@gmail.com>
2026-09-03 17:11:12 +0700

    Update AdminProducts.jsx

--- src/pages/AdminProducts.jsx
+++ src/pages/AdminProducts.jsx
@@ -22,6 +22,7 @@ function AdminProducts({ onNavigate, onLogoClick, currentUser, onLogout }) {
   const [newCategoryName, setNewCategoryName] = useState("");
   const [addingCategory, setAddingCategory] = useState(false);
   const [deletingCategoryId, setDeletingCategoryId] = useState(null);
+  const [activeTab, setActiveTab] = useState("products");
 
   const loadProducts = () => {
     setLoading(true);
@@ -237,6 +238,30 @@ function AdminProducts({ onNavigate, onLogoClick, currentUser, onLogout }) {
       />
 
       <div className="admin-page">
+
+  <div className="admin-tabs">
+    <button
+      className={
+        "admin-tab" +
+        (activeTab === "products" ? " admin-tab-active" : "")
+      }
+      onClick={() => setActiveTab("products")}
+    >
+      จัดการสินค้า
+    </button>
+
+    <button
+      className={
+        "admin-tab" +
+        (activeTab === "categories" ? " admin-tab-active" : "")
+      }
+      onClick={() => setActiveTab("categories")}
+    >
+      จัดการหมวดหมู่สินค้า
+    </button>
+  </div>
+        {activeTab === "categories" && (
+<>
         <h2 className="admin-title">จัดการหมวดหมู่สินค้า</h2>
 
         <form className="auth-form event-form" onSubmit={handleAddCategory}>
@@ -278,7 +303,11 @@ function AdminProducts({ onNavigate, onLogoClick, currentUser, onLogout }) {
             ))}
           </div>
         )}
+  </>
+)}
 
+        {activeTab === "products" && (
+<>
         <div className="admin-events-header admin-section-spacing">
           <h2 className="admin-title">จัดการสินค้า</h2>
           <button
@@ -383,6 +412,11 @@ function AdminProducts({ onNavigate, onLogoClick, currentUser, onLogout }) {
       </div>
     </>
   );
+              </>
+      )}
+    </div>
+  </>
+);
 }
 
 export default AdminProducts;
